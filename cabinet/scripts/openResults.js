256dc004fdc280101d17559a8be727fac41818ae
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-07-15 19:00:27 +0200

    url

--- cabinet/scripts/openResults.js
+++ cabinet/scripts/openResults.js
@@ -1,10 +1,10 @@
 // Открытие результатов
 function openResults(resultId) {
     resultId = resultId.split("-")
-    window.open(`/results/?id=${resultId[1]}_${resultId[3]}`, "_blank")
+    window.open(`./results/?id=${resultId[1]}_${resultId[3]}`, "_blank")
 }
 
 // Открытие всех результатов
 $("#open-all-results").on("click tap", () => {
-    window.open(`/results/?id=${openedCardId}`, "_blank")
+    window.open(`./results/?id=${openedCardId}`, "_blank")
 })
