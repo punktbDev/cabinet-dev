8ab4c63d31570678443aadcd3425017e85e9a6e7
MaSStiK <mathew.ovchinnikov@gmail.com>
2025-07-15 18:31:00 +0200

    url

--- admin/scripts/auth.js
+++ admin/scripts/auth.js
@@ -2,15 +2,15 @@
 let userData = JSON.parse(localStorage.getItem("userData"))
 
 if (userData === null) {
-    location.href = "/login/"
+    location.href = "login"
 }
 
 // Если нету логина или пароля или secret, перекидывает на вход
 if (!userData.hasOwnProperty("login") || !userData.hasOwnProperty("password")) {
     localStorage.removeItem("userData")
-    location.href = "/login/"
+    location.href = "login"
 }
 
 if (!userData?.is_admin) {
-    location.href = "/"
+    location.href = "../"
 }
